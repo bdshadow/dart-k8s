7e389ab186b2030e9a46630d84e38fcc4237fbe7
Dmitrii Bocharov <bdshadow@gmail.com>
2019-12-05 19:04:32 +0100

    fix - no namespaces found (e.g. not enough rights)
    Signed-off-by: Dmitrii Bocharov <bdshadow@gmail.com>

--- lib/resource/namespace_list.dart
+++ lib/resource/namespace_list.dart
@@ -2,11 +2,14 @@ part of dart_k8s_client;
 
 class NamespaceList {
   static const ResourceKind kind = ResourceKind.NamespaceList;
-  Set<Namespace> items = HashSet<Namespace>();
+  Set<Namespace> items;
 
   NamespaceList(this.items);
 
   factory NamespaceList.fromJson(Map<String, dynamic> json) {
+    if (json["items"] == null) {
+      return NamespaceList(HashSet<Namespace>());
+    }
     return NamespaceList(Set<Namespace>.from(json["items"].map((i) => Namespace.fromJson(i))));
   }
 }
